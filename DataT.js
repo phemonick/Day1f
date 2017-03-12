7365166c6a0adcdf4d12629b18cd19e694394419
omowunmi Elebute <omowunmi Elebute>
2017-03-12 19:32:49 -0400

    firt test, all failed

---
+++ DataT.js
@@ -0,0 +1,35 @@
+function dataType(inpu){
+
+	if (){
+
+	return "no value";
+
+}
+else if(){
+
+	return inpu ;
+}
+
+else if (){
+
+	return val ;
+}
+
+else if (){
+
+	return len;
+}
+
+else if (){
+
+	return inpu;
+}
+else if (){
+
+	return inpu;
+}
+}
+
+module.exports = {
+	dataType : dataType
+}
